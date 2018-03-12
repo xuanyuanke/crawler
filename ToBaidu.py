ac816e1e3efb6fbaaca603de6b035827913770aa
xuanyuanke <xuanyuanke_1988@163.com>
2018-03-13 00:05:59 +0800

    关闭界面

--- ToBaidu.py
+++ ToBaidu.py
@@ -15,7 +15,7 @@ class ToBaidu:
 		# chrome_options.add_argument('--disable-gpu') #谷歌文档提到需要加上这个属性来规避bug
 		# chrome_options.add_argument('--hide-scrollbars') #隐藏滚动条, 应对一些特殊页面
 		# chrome_options.add_argument('blink-settings=imagesEnabled=false') #不加载图片, 提升速度
-		# chrome_options.add_argument('--headless') #浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+		chrome_options.add_argument('--headless') #浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
 		chrome_options.add_argument("--proxy-server=http://" + ip) 
 		driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 		#(executable_path='/usr/local/bin/chromedriver')
